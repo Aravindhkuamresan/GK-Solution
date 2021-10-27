841089538570e4eccfa718cfb60a884ad2402c3a
Aravindh K <aravindh-5615@csez.zohocorpin.com>
2021-10-27 23:59:14 +0530

    handle review content slide

--- scripts/eventhandler.js
+++ scripts/eventhandler.js
@@ -1,3 +1,17 @@
+function toggleMenu(element) {
+	element.classList.toggle("change"); //No I18N
+}
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("review-content");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides,10000); // Change image every 10 seconds
+}
 jQuery(".mobmenu-slide,.menu-open").on('click touchstart', function(e) {
 	e.stopPropagation();
 }); 
@@ -9,6 +23,3 @@ jQuery('.menu-open, a.contactus').click(function(e) {
 		$overlay.addClass('active');
 	}
 });
-function toggleMenu(element) {
-	element.classList.toggle("change"); //No I18N
-}
