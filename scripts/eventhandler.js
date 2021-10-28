c3e4179c5a5b5774604d2f695692f4ce0da73b21
Aravindh K <aravindh-5615@csez.zohocorpin.com>
2021-10-28 21:12:26 +0530

    UI fixes

--- scripts/eventhandler.js
+++ scripts/eventhandler.js
@@ -2,33 +2,8 @@ function toggleMenu(element) {
 	element.classList.toggle("change"); //No I18N
 }
 
-var slideIndex = 0;
-function initReviewSlides() {
-  var i;
-  var slides = document.getElementsByClassName("review-content");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(initReviewSlides,3000);
-}
-var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "109976498145369");
-      chatbox.setAttribute("attribution", "biz_inbox");
-
-      window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v12.0'
-        });
-      };
-
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
+jQuery(document).ready(function(){
+	jQuery('#contact').on('click',function(){
+			window.scrollTo(0,document.body.scrollHeight);
+	});
+});
