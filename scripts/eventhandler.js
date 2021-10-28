6b7b2eb65314c93c3d7b86a16a080dfae62d43fc
Aravindh K <aravindh-5615@csez.zohocorpin.com>
2021-10-28 10:26:24 +0530

    UI fixes

--- scripts/eventhandler.js
+++ scripts/eventhandler.js
@@ -1,7 +1,9 @@
 function toggleMenu(element) {
 	element.classList.toggle("change"); //No I18N
 }
-function showSlides() {
+
+var slideIndex = 0;
+function initReviewSlides() {
   var i;
   var slides = document.getElementsByClassName("review-content");
   for (i = 0; i < slides.length; i++) {
@@ -10,16 +12,20 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides,10000); // Change image every 10 seconds
+  setTimeout(initReviewSlides,3000);
 }
-jQuery(".mobmenu-slide,.menu-open").on('click touchstart', function(e) {
-	e.stopPropagation();
-}); 
-jQuery('.menu-open, a.contactus').click(function(e) {
-	var $overlay = jQuery('.mobmenu-slide, .ovrly');  //no i18n
-	if ($overlay.hasClass('active')) {
-		$overlay.removeClass('active');
-	}else {	
-		$overlay.addClass('active');
-	}
+
+jQuery(document).ready(function(){
+	initReviewSlides();
+	jQuery(".mobmenu-slide,.menu-open").on('click touchstart', function(e) {
+		e.stopPropagation();
+	}); 
+	jQuery('.menu-open, a.contactus').click(function(e) {
+		var $overlay = jQuery('.mobmenu-slide, .ovrly');  //no i18n
+		if ($overlay.hasClass('active')) {
+			$overlay.removeClass('active');
+		}else {	
+			$overlay.addClass('active');
+		}	
+	});
 });
