ec4f79b225397edc72258ac0c539eaf8d39b4d0c
Aravindh K <aravindh-5615@csez.zohocorpin.com>
2021-10-29 16:52:57 +0530

    Mobile UI block

--- scripts/eventhandler.js
+++ scripts/eventhandler.js
@@ -7,10 +7,17 @@ jQuery(document).ready(function(){
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
-	/*jQuery('#contact').on('click',function(){
-			jQuery("html, body").animate({ scrollTop:document.body.scrollHeight},"fast");
-	});
-	jQuery('#home').on('click',function(){
-			 jQuery("html, body").animate({ scrollTop: 0},"fast");
-	});*/
+
+var userAgent  = navigator.userAgent;
+	 	var isMobile = false;
+		if(userAgent.indexOf("Android")!=-1 ||  userAgent.indexOf("iPhone")!=-1 ||  userAgent.indexOf("iPad")!=-1 ){
+			var $overlay = jQuery('.ovrly'); //no i18n
+		if ($overlay.hasClass('active')) {
+			$overlay.removeClass('active');
+		} else {
+			$overlay.addClass('active');
+		}		
+		}else{
+			jQuery('.ovrly').hide();
+		}
 });
